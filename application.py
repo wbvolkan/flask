826483da1dac5c1414f6e0f65a149dc582842c81
wbvolkan <56910588+wbvolkan@users.noreply.github.com>
2020-12-12 19:40:07 +0300

    Update application.py

--- application.py
+++ application.py
@@ -37,6 +37,7 @@ def index():
 		data.append(solafis)
 		data.append(tv)
 		data.append(logo)
+		return url + " " + solafis
 		urllib.request.urlretrieve(url,solafis+".png")
 		stand1(solafis, solafis, solafis, solafis, solafis_x, solafis_y, saafis_x, saafis_y, tv_x, tv_y, logo_x, logo_y)
 		return "İşlem başarılı... /photo adresinde fotoğrafı görebilirsiniz."
